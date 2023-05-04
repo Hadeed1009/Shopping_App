6e97e9f57f8316f5f2a20c969447f597caf0b8ad
Hadeed Naeem <hadeednaeem10@gmail.com>
2023-05-04 19:29:59 +0500

    Horizontal layout added in home

---
+++ app/src/main/java/com/example/laylo/Modelss/HomeModel.java
@@ -0,0 +1,29 @@
+package com.example.laylo.Modelss;
+
+import android.widget.ImageView;
+
+public class HomeModel {
+    int image;
+    String text;
+
+    public HomeModel(int image, String text) {
+        this.image = image;
+        this.text = text;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
